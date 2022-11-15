29d732277ceafbccfc1e6682a7ef411497cceb56
sihyeong671 <bshlab671@naver.com>
2022-11-15 15:52:39 +0000

    feat: add checkbox
    - you can choose categories what you want to see

--- streamlit/data_vz.py
+++ streamlit/data_vz.py
@@ -1,9 +1,18 @@
 import streamlit as st
-import matplotlib.pyplot as plt
-import pandas as pd
 import cv2
 import json
 import os
+from typing import List
+
+
+def make_checkbox(class_list: List[tuple]) -> List[int]:
+    check_boxes = st.columns(5)
+    return_list = [False]*len(class_list)
+    for idx, (class_name, class_id) in enumerate(class_list):
+        with check_boxes[idx%5]:
+            check = st.checkbox(class_name, value=True)
+        return_list[class_id] = check
+    return return_list
 
 
 def run():
@@ -17,33 +26,33 @@ def run():
     data_images = data['images']
     data_annotations = data['annotations']
     data_categories = data['categories']
-    classes = [c['name'] for c in data_categories]
-
+    classes = [(c['name'], c['id']) for c in data_categories]
 
     img_path, img_id = st.selectbox(
         'choose image',
         [(img['file_name'], img['id']) for img in data_images],
     )
 
+    check_list = make_checkbox(classes)
+
     st.write(f'img_path: {img_path} img_id: {img_id}')
 
     img = cv2.imread(os.path.join('../dataset/', img_path))
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
- 
     
     # get bbox
     bboxes = []
     for anno in data_annotations:
         if anno['image_id'] == img_id:
-            bboxes.append(anno['bbox'])
+            bboxes.append((anno['bbox'], anno['category_id']))
     
     # draw bbox
     RED_COLOR = (255, 0, 0)
     LINE_WEIGHT = 2
-    for bbox in bboxes:
-        x, y, width, height = map(int, bbox)
-        img = cv2.rectangle(img, (x, y), (x+width, y+height), RED_COLOR, LINE_WEIGHT)
+    for bbox, c_id in bboxes:
+        if check_list[c_id]:
+            x, y, width, height = map(int, bbox)
+            img = cv2.rectangle(img, (x, y), (x+width, y+height), RED_COLOR, LINE_WEIGHT)
 
     st.image(img)
 
