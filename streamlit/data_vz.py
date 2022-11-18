abd96f68c5a40dc6b8127dfa3c05542689992651
Nask <tjdrms0459@naver.com>
2022-11-18 12:12:57 +0000

    refactor & feat

--- streamlit/data_vz.py
+++ streamlit/data_vz.py
@@ -8,6 +8,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from typing import List
 import sys
+from collections import Counter
 
 sys.path.append("../")
 from utils.streamlit_utils import *
@@ -93,51 +94,49 @@ def make_bbox_count_tab(df: pd.DataFrame):
     """
     st.header("bbox_count")
 
-    nums = []
-    max_bbox = 0
-    max_img = 0
-    for image_id in df["image_id"].unique():
-        num = len(df[df["image_id"] == image_id])
-        nums.append(num)
-        if num > max_bbox:
-            max_img = image_id
-            max_bbox = num
+    bbox_nums_dict = dict(df["image_id"].value_counts())
+    bbox_nums = list(bbox_nums_dict.values())
+    bbox_min = min(bbox_nums)
+    bbox_max = max(bbox_nums)
 
-    dic = dict()
-    for num in nums:
-        if num not in dic.keys():
-            dic[num] = 1
-        else:
-            dic[num] += 1
-    mode = sorted(dic.items(), key=lambda x: x[1])[-1][0]
-
-    st.write(f"min_bbox_num: {min(nums)}")
-    st.write(f"max_bbox_num: {max(nums)}")
-    st.write(f"mode_bbox_num: {mode}")
-    st.write(f"max_bbox_img: {max_img}")
-
-    fig, axes = plt.subplots(3, 1, figsize=(12, 24))
-    ax0 = axes[0]
-    ax1 = axes[1]
-    ax2 = axes[2]
-
-    ax0.hist(nums, bins=70)
-    ax0.set_title("total_distribution")
-    ax0.set_xlabel("bbox_num")
-    ax0.set_ylabel("image_num")
-
-    ax1.hist(nums, bins=20, range=(0, 21))
-    ax1.set_title("1~20_distribution")
-    ax1.set_xlabel("bbox_num")
-    ax1.set_ylabel("image_num")
-
-    ax2.hist(nums, bins=50, range=(21, max(nums)))
-    ax2.set_title("over21_distribution")
-    ax2.set_xlabel("bbox_num")
-    ax2.set_ylabel("image_num")
-
-    plt.tight_layout(h_pad=10)
+    max_bbox_img = sorted(bbox_nums_dict.items(), key=lambda x: x[1])[-1][0]
+    most_common = Counter(bbox_nums).most_common()[0]
+    bbox_mode = most_common[0]
+    bbox_mode_img_num = most_common[1]
+    fig = plt.figure(figsize=(12, 8))
+    plt.hist(bbox_nums, bins=70)
     st.pyplot(fig)
+
+    st.write(f"min_bbox_num: {bbox_min}")
+    st.write(f"max_bbox_num: {bbox_max}")
+    st.write(f"mode_bbox_num: {bbox_mode}")
+    st.write(f"max_bbox_img: {max_bbox_img}")
+    st.write(f"bbox_mode_img_num: {bbox_mode_img_num}")
+
+    # fig, axes = plt.subplots(3, 1, figsize=(12, 24))
+    # ax0 = axes[0]
+    # ax1 = axes[1]
+    # ax2 = axes[2]
+
+
+#
+# ax0.hist(nums, bins=70)
+# ax0.set_title("total_distribution")
+# ax0.set_xlabel("bbox_num")
+# ax0.set_ylabel("image_num")
+#
+# ax1.hist(nums, bins=20, range=(0, 21))
+# ax1.set_title("1~20_distribution")
+# ax1.set_xlabel("bbox_num")
+# ax1.set_ylabel("image_num")
+#
+# ax2.hist(nums, bins=50, range=(21, max(nums)))
+# ax2.set_title("over21_distribution")
+# ax2.set_xlabel("bbox_num")
+# ax2.set_ylabel("image_num")
+#
+# plt.tight_layout(h_pad=10)
+# st.pyplot(fig)
 
 
 # 실행 명령어 streamlit run data_vz.py  --server.fileWatcherType none --server.port 30004
