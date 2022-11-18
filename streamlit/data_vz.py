63ffcfa9c7dcd54f9ff3f9fcb99acf6cd2f2b2a3
Nask <tjdrms0459@naver.com>
2022-11-18 11:08:14 +0000

    refactor & feat

--- streamlit/data_vz.py
+++ streamlit/data_vz.py
@@ -6,105 +6,6 @@ import numpy as np
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
-<<<<<<< HEAD
-from pycocotools.coco import COCO
-from collections import defaultdict
-from typing import List, Tuple
-
-CLASSES = [
-    "General trash",
-    "Paper",
-    "Paper pack",
-    "Metal",
-    "Glass",
-    "Plastic",
-    "Styrofoam",
-    "Plastic bag",
-    "Battery",
-    "Clothing",
-]
-RED_COLOR = (255, 0, 0)
-LINE_WEIGHT = 2
-
-
-def set_data() -> pd.DataFrame:
-    coco = COCO("../dataset/train.json")
-
-    df = pd.DataFrame()
-
-    image_ids = []
-    class_name = []
-    class_id = []
-    x_min = []
-    y_min = []
-    x_max = []
-    y_max = []
-
-    for image_id in coco.getImgIds():
-
-        image_info = coco.loadImgs(image_id)[0]
-        ann_ids = coco.getAnnIds(imgIds=image_info["id"])
-        anns = coco.loadAnns(ann_ids)
-
-        file_name = image_info["file_name"]
-
-        for ann in anns:
-            image_ids.append(file_name)
-            class_name.append(CLASSES[ann["category_id"]])
-            class_id.append(ann["category_id"])
-            x_min.append(float(ann["bbox"][0]))
-            y_min.append(float(ann["bbox"][1]))
-            x_max.append(float(ann["bbox"][0]) + float(ann["bbox"][2]))
-            y_max.append(float(ann["bbox"][1]) + float(ann["bbox"][3]))
-
-    df["image_id"] = image_ids
-    df["class_name"] = class_name
-    df["class_id"] = class_id
-    df["x_min"] = x_min
-    df["y_min"] = y_min
-    df["x_max"] = x_max
-    df["y_max"] = y_max
-    return df
-
-
-def make_checkbox(class_list: List[str], id_list: List[int]) -> List[bool]:
-    check_boxes = st.columns(5)
-    return_list = [False] * len(class_list)
-    for idx, (class_name, class_id) in enumerate(zip(class_list, id_list)):
-        with check_boxes[idx % 5]:
-            check = st.checkbox(class_name, value=True)
-        return_list[class_id] = check
-    return return_list
-
-
-def get_bbox(img_group: pd.DataFrame) -> List[list]:
-    bboxes = []
-    for idx, row in img_group.iterrows():
-        c_id, x_min, y_min, x_max, y_max = (
-            row.class_id,
-            row.x_min,
-            row.y_min,
-            row.x_max,
-            row.y_max,
-        )
-        bboxes.append([c_id, x_min, y_min, x_max, y_max])
-    return bboxes
-
-
-def draw_bbox(img: np.array, bboxes: List[list], check_list: List[bool]) -> np.array:
-    for bbox in bboxes:
-        c_id, x_min, y_min, x_max, y_max = map(int, bbox)
-        if check_list[c_id]:
-            img = cv2.rectangle(
-                img, (x_min, y_min), (x_max, y_max), RED_COLOR, LINE_WEIGHT
-            )
-    return img
-
-
-def make_vz_tab(df: pd.DataFrame):
-    """
-    사진 한 장씩 선택해서 원하는 카테고리의 bbox를 선택해서 볼 수 있는 탭
-=======
 from typing import List
 import sys
 
@@ -116,7 +17,6 @@ def label_fix_tab(df: pd.DataFrame):
     """사진 한 장씩 선택해서 원하는 카테고리의 bbox를 선택해서 볼 수 있는 탭
     Args:
         df: coco dataset의 annotations를 각 행으로 하는 데이터 프레임
->>>>>>> f1b1ad03c94ee631bc7a8181965ac203f977992f
     """
     st.header("Data Analysis")
 
@@ -125,14 +25,7 @@ def label_fix_tab(df: pd.DataFrame):
 
     img_path = st.selectbox("choose image", img_paths)
 
-<<<<<<< HEAD
-    check_list = make_checkbox(
-        class_list=df.class_name.unique().tolist(),
-        id_list=df.class_id.unique().tolist(),
-    )
-=======
     check_list = make_checkbox(id_list=df.class_id.unique().tolist())
->>>>>>> f1b1ad03c94ee631bc7a8181965ac203f977992f
 
     st.write(f"img_path: {img_path}")
 
@@ -142,9 +35,6 @@ def label_fix_tab(df: pd.DataFrame):
     bboxes = get_bbox(group.get_group(img_path))
     img = draw_bbox(img, bboxes, check_list)
 
-<<<<<<< HEAD
-    st.image(img)
-=======
     col1, col2 = st.columns([1, 3])
     if "state" not in st.session_state:
         st.session_state.state = False
@@ -180,7 +70,6 @@ def label_fix_tab(df: pd.DataFrame):
 
     if st.session_state.state:
         change_label(selected_id, selected_item)
->>>>>>> f1b1ad03c94ee631bc7a8181965ac203f977992f
 
 
 def make_category_count_tab(df: pd.DataFrame):
@@ -188,9 +77,7 @@ def make_category_count_tab(df: pd.DataFrame):
     Args:
         df: coco dataset의 annotations를 각 행으로 하는 데이터 프레임
     """
-<<<<<<< HEAD
-    카테고리 별 bbox 갯수 시각화
-    """
+
     st.header("category_count")
     fig = plt.figure(figsize=(12, 8))
     sns.countplot(x=df.class_name)
@@ -198,27 +85,23 @@ def make_category_count_tab(df: pd.DataFrame):
 
 
 def make_bbox_count_tab(df: pd.DataFrame):
+
     """
     이미지 별 bbox 갯수 시각화
+    Args:
+        df: coco dataset의 annotations를 각 행으로 하는 데이터 프레임
     """
+    st.header("bbox_count")
 
-    coco = COCO("../dataset/train.json")
     nums = []
     max_bbox = 0
     max_img = 0
-    for image_id in coco.getImgIds():
-        image_info = coco.loadImgs(image_id)[0]
-        ann_ids = coco.getAnnIds(imgIds=image_info["id"])
-        anns = coco.loadAnns(ann_ids)
-        if len(anns) > max_bbox:
+    for image_id in df["image_id"].unique():
+        num = len(df[df["image_id"] == image_id])
+        nums.append(num)
+        if num > max_bbox:
             max_img = image_id
-            max_bbox = len(anns)
-        nums.append(len(anns))
-    fig = plt.figure(figsize=(12, 8))
-    plt.hist(nums, bins=70)
-    plt.title("total_distribution")
-    plt.xlabel("bbox_num")
-    plt.ylabel("image_num")
+            max_bbox = num
 
     dic = dict()
     for num in nums:
@@ -226,52 +109,41 @@ def make_bbox_count_tab(df: pd.DataFrame):
             dic[num] = 1
         else:
             dic[num] += 1
-
     mode = sorted(dic.items(), key=lambda x: x[1])[-1][0]
 
-=======
-    st.header("category_count")
-    fig = plt.figure(figsize=(12, 8))
-    sns.countplot(x=df.class_name)
->>>>>>> f1b1ad03c94ee631bc7a8181965ac203f977992f
-    st.pyplot(fig)
-    st.write(f"max_bbox_num: {max(nums)}")
     st.write(f"min_bbox_num: {min(nums)}")
+    st.write(f"max_bbox_num: {max(nums)}")
     st.write(f"mode_bbox_num: {mode}")
     st.write(f"max_bbox_img: {max_img}")
 
-    fig = plt.figure(figsize=(12, 8))
-    plt.hist(nums, bins=20, range=(0, 21))
-    plt.title("1~20_distribution")
-    plt.xlabel("bbox_num")
-    plt.ylabel("image_num")
+    fig, axes = plt.subplots(3, 1, figsize=(12, 24))
+    ax0 = axes[0]
+    ax1 = axes[1]
+    ax2 = axes[2]
+
+    ax0.hist(nums, bins=70)
+    ax0.set_title("total_distribution")
+    ax0.set_xlabel("bbox_num")
+    ax0.set_ylabel("image_num")
+
+    ax1.hist(nums, bins=20, range=(0, 21))
+    ax1.set_title("1~20_distribution")
+    ax1.set_xlabel("bbox_num")
+    ax1.set_ylabel("image_num")
+
+    ax2.hist(nums, bins=50, range=(21, max(nums)))
+    ax2.set_title("over21_distribution")
+    ax2.set_xlabel("bbox_num")
+    ax2.set_ylabel("image_num")
+
+    plt.tight_layout(h_pad=10)
     st.pyplot(fig)
 
-    fig = plt.figure(figsize=(12, 8))
-    plt.hist(nums, bins=50, range=(21, max(nums)))
-    plt.title("over21_distribution")
-    plt.xlabel("bbox_num")
-    plt.ylabel("image_num")
-    st.pyplot(fig)
-
-
-<<<<<<< HEAD
-# 박스 크기
-
-# 사진별 카테고리 포함 여부 분포
-
-#
-
-# 실행 명령어 streamlit run data_vz.py  --server.fileWatcherType none --server.port 30004
-st.title("Data Visualization")
-vz_tab, count_tab, bbox_count_tab = st.tabs(["analysis", "count", "bbox_count"])
-=======
 
 # 실행 명령어 streamlit run data_vz.py  --server.fileWatcherType none --server.port 30004
 st.set_page_config(layout="wide")
 st.title("Data Visualization")
-vz_tab, count_tab = st.tabs(["analysis", "count"])
->>>>>>> f1b1ad03c94ee631bc7a8181965ac203f977992f
+vz_tab, count_tab, bbox_count_tab = st.tabs(["analysis", "count", "bbox_count"])
 df = set_data()
 with vz_tab:
     label_fix_tab(df)
