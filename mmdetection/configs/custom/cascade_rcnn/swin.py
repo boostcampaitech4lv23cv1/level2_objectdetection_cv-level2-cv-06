3fb898773655483832fb2d4feb7689b66114ada8
JuHeon Oh <ojhgold@gmail.com>
2022-11-22 14:32:11 +0900

    refactor: change file name
    remove cascade rcnn and leave only model name

---
+++ mmdetection/configs/custom/cascade_rcnn/swin.py
@@ -0,0 +1,246 @@
+_base_ = ["../custom/custom_dataset.py", "../_base_/default_runtime.py"]
+
+pretrained = "https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_base_patch4_window7_224.pth"  # noqa
+
+model = dict(
+    type="CascadeRCNN",
+    backbone=dict(
+        # _delete_=True,
+        type="SwinTransformer",
+        embed_dims=128,
+        depths=[2, 2, 18, 2],
+        num_heads=[4, 8, 16, 32],
+        window_size=7,
+        mlp_ratio=4,
+        qkv_bias=True,
+        qk_scale=None,
+        drop_rate=0.0,
+        attn_drop_rate=0.0,
+        drop_path_rate=0.2,
+        patch_norm=True,
+        out_indices=(0, 1, 2, 3),
+        with_cp=False,
+        convert_weights=True,
+        init_cfg=dict(type="Pretrained", checkpoint=pretrained),
+    ),
+    neck=dict(
+        type="FPN", in_channels=[128, 256, 512, 1024], out_channels=256, num_outs=5
+    ),
+    rpn_head=dict(
+        type="RPNHead",
+        in_channels=256,
+        feat_channels=256,
+        anchor_generator=dict(
+            type="AnchorGenerator",
+            scales=[8],
+            ratios=[0.5, 1.0, 2.0],
+            strides=[4, 8, 16, 32, 64],
+        ),
+        bbox_coder=dict(
+            type="DeltaXYWHBBoxCoder",
+            target_means=[0.0, 0.0, 0.0, 0.0],
+            target_stds=[1.0, 1.0, 1.0, 1.0],
+        ),
+        loss_cls=dict(type="CrossEntropyLoss", use_sigmoid=True, loss_weight=1.0),
+        # loss_cls=dict(type="FocalLoss", use_sigmoid=True, loss_weight=1.0),
+        loss_bbox=dict(type="SmoothL1Loss", beta=1.0 / 9.0, loss_weight=1.0),
+    ),
+    roi_head=dict(
+        type="CascadeRoIHead",
+        num_stages=3,
+        stage_loss_weights=[1, 0.5, 0.25],
+        bbox_roi_extractor=dict(
+            type="SingleRoIExtractor",
+            roi_layer=dict(type="RoIAlign", output_size=7, sampling_ratio=0),
+            out_channels=256,
+            featmap_strides=[4, 8, 16, 32],
+        ),
+        bbox_head=[
+            dict(
+                type="Shared2FCBBoxHead",
+                in_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=10,
+                bbox_coder=dict(
+                    type="DeltaXYWHBBoxCoder",
+                    target_means=[0.0, 0.0, 0.0, 0.0],
+                    target_stds=[0.1, 0.1, 0.2, 0.2],
+                ),
+                reg_class_agnostic=True,
+                loss_cls=dict(
+                    type="CrossEntropyLoss", use_sigmoid=False, loss_weight=1.0
+                ),
+                # loss_cls=dict(type="FocalLoss", use_sigmoid=False, loss_weight=1.0),
+                loss_bbox=dict(type="SmoothL1Loss", beta=1.0, loss_weight=1.0),
+            ),
+            dict(
+                type="Shared2FCBBoxHead",
+                in_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=10,
+                bbox_coder=dict(
+                    type="DeltaXYWHBBoxCoder",
+                    target_means=[0.0, 0.0, 0.0, 0.0],
+                    target_stds=[0.05, 0.05, 0.1, 0.1],
+                ),
+                reg_class_agnostic=True,
+                loss_cls=dict(
+                    type="CrossEntropyLoss", use_sigmoid=False, loss_weight=1.0
+                ),
+                # loss_cls=dict(type="FocalLoss", use_sigmoid=False, loss_weight=1.0),
+                loss_bbox=dict(type="SmoothL1Loss", beta=1.0, loss_weight=1.0),
+            ),
+            dict(
+                type="Shared2FCBBoxHead",
+                in_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=10,
+                bbox_coder=dict(
+                    type="DeltaXYWHBBoxCoder",
+                    target_means=[0.0, 0.0, 0.0, 0.0],
+                    target_stds=[0.033, 0.033, 0.067, 0.067],
+                ),
+                reg_class_agnostic=True,
+                loss_cls=dict(
+                    type="CrossEntropyLoss", use_sigmoid=False, loss_weight=1.0
+                ),
+                # loss_cls=dict(type="FocalLoss", use_sigmoid=False, loss_weight=1.0),
+                loss_bbox=dict(type="SmoothL1Loss", beta=1.0, loss_weight=1.0),
+            ),
+        ],
+    ),
+    # model training and testing settings
+    train_cfg=dict(
+        rpn=dict(
+            assigner=dict(
+                type="MaxIoUAssigner",
+                pos_iou_thr=0.7,
+                neg_iou_thr=0.3,
+                min_pos_iou=0.3,
+                match_low_quality=True,
+                ignore_iof_thr=-1,
+            ),
+            sampler=dict(
+                type="RandomSampler",
+                num=256,
+                pos_fraction=0.5,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=False,
+            ),
+            allowed_border=0,
+            pos_weight=-1,
+            debug=False,
+        ),
+        rpn_proposal=dict(
+            nms_pre=2000,
+            max_per_img=2000,
+            nms=dict(type="nms", iou_threshold=0.7),
+            min_bbox_size=0,
+        ),
+        rcnn=[
+            dict(
+                assigner=dict(
+                    type="MaxIoUAssigner",
+                    pos_iou_thr=0.5,
+                    neg_iou_thr=0.5,
+                    min_pos_iou=0.5,
+                    match_low_quality=False,
+                    ignore_iof_thr=-1,
+                ),
+                sampler=dict(
+                    type="RandomSampler",
+                    num=512,
+                    pos_fraction=0.25,
+                    neg_pos_ub=-1,
+                    add_gt_as_proposals=True,
+                ),
+                pos_weight=-1,
+                debug=False,
+            ),
+            dict(
+                assigner=dict(
+                    type="MaxIoUAssigner",
+                    pos_iou_thr=0.6,
+                    neg_iou_thr=0.6,
+                    min_pos_iou=0.6,
+                    match_low_quality=False,
+                    ignore_iof_thr=-1,
+                ),
+                sampler=dict(
+                    type="RandomSampler",
+                    num=512,
+                    pos_fraction=0.25,
+                    neg_pos_ub=-1,
+                    add_gt_as_proposals=True,
+                ),
+                pos_weight=-1,
+                debug=False,
+            ),
+            dict(
+                assigner=dict(
+                    type="MaxIoUAssigner",
+                    pos_iou_thr=0.7,
+                    neg_iou_thr=0.7,
+                    min_pos_iou=0.7,
+                    match_low_quality=False,
+                    ignore_iof_thr=-1,
+                ),
+                sampler=dict(
+                    type="RandomSampler",
+                    num=512,
+                    pos_fraction=0.25,
+                    neg_pos_ub=-1,
+                    add_gt_as_proposals=True,
+                ),
+                pos_weight=-1,
+                debug=False,
+            ),
+        ],
+    ),
+    test_cfg=dict(
+        rpn=dict(
+            nms_pre=1000,
+            max_per_img=1000,
+            nms=dict(type="nms", iou_threshold=0.7),
+            min_bbox_size=0,
+        ),
+        rcnn=dict(
+            score_thr=0.05,
+            nms=dict(type="soft_nms", iou_threshold=0.5),
+            max_per_img=100,
+        ),
+    ),
+)
+
+
+optimizer = dict(
+    type="AdamW",
+    lr=0.0001,
+    betas=(0.9, 0.999),
+    weight_decay=0.05,
+    paramwise_cfg=dict(
+        custom_keys=dict(
+            absolute_pos_embed=dict(decay_mult=0.0),
+            relative_position_bias_table=dict(decay_mult=0.0),
+            norm=dict(decay_mult=0.0),
+        )
+    ),
+)
+optimizer_config = dict(grad_clip=None)
+lr_config = dict(
+    policy="step",
+    warmup="linear",
+    warmup_iters=1000,
+    warmup_ratio=0.001,
+    step=[8, 15],
+)
+
+
+# fp16 = dict(loss_scale="dynamic")
+runner = dict(
+    type="EpochBasedRunner",  # Type of runner to use (i.e. IterBasedRunner or EpochBasedRunner)
+    max_epochs=20,
+)  # Runner that runs the workflow in total max_epochs. For IterBasedRunner use `max_iters`
