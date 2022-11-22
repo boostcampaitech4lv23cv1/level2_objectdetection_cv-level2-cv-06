8c7c0a3c24a4498f0015153a44792e05f32d43bf
sihyeong671 <bshlab671@naver.com>
2022-11-22 06:38:08 +0000

    refactor & add: seperate scheduler
    - add _base_ param in regnet and remove other params
    - add base scheduler
    close #40

--- mmdetection/configs/custom/cascade_rcnn/pvt_b1.py
+++ mmdetection/configs/custom/cascade_rcnn/pvt_b1.py
@@ -2,6 +2,7 @@ _base_ = [
     "../../_base_/models/cascade_rcnn_r50_fpn.py",
     "../_base_/dataset.py",
     "../_base_/runtime.py",
+    "../_base_/base_scheduler.py",
 ]
 
 model = dict(
@@ -16,30 +17,4 @@ model = dict(
         ),
     ),
     neck=dict(type="FPN", in_channels=[64, 128, 320, 512]),
-)
-
-optimizer = dict(
-    type="AdamW",
-    lr=0.0001,
-    betas=(0.9, 0.999),
-    weight_decay=0.05,
-    paramwise_cfg=dict(
-        custom_keys=dict(
-            absolute_pos_embed=dict(decay_mult=0.0),
-            relative_position_bias_table=dict(decay_mult=0.0),
-            norm=dict(decay_mult=0.0),
-        )
-    ),
-)
-optimizer_config = dict(grad_clip=None)
-lr_config = dict(
-    policy="step",
-    warmup="linear",
-    warmup_iters=1000,
-    warmup_ratio=0.001,
-    step=[8, 15],
-)
-runner = dict(
-    type="EpochBasedRunner",
-    max_epochs=20,
 )
