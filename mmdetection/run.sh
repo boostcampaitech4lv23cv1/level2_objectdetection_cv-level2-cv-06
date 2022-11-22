d419c982b1e03e88eb9113cbd9cc13284809aefe
JuHeon Oh <ojhgold@gmail.com>
2022-11-22 15:26:06 +0900

    feat: closed #39

--- mmdetection/run.sh
+++ mmdetection/run.sh
@@ -1,3 +1,5 @@
-python tools/train.py \
-    configs/custom/cascade_rcnn/cascade_rcnn_regnet.py \
-    
+# python tools/train.py configs/custom/cascade_rcnn/pvt_b1.py --work-dir work_dirs/cascade_rcnn_pvt_b1 --seed 42
+
+python tools/train.py configs/custom/cascade_rcnn/pvt_b1.py \
+--wandb_nm cascade_rcnn_pvt_b1 --wandb_tag pvt cascade_rcnn
+
