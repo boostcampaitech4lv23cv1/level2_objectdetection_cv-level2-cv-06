885c340e604a05363fafa9c9ebf745d7c20c607d
sshinohs <sshinohs@gmail.com>
2022-11-23 05:21:10 +0000

    test: cascadercnn r101

--- mmdetection/configs/custom/run.sh
+++ mmdetection/configs/custom/run.sh
@@ -1,3 +1,5 @@
+## cascadercnn_r50 experiment
+
 # python tools/train.py \
 #     configs/custom/cascade_rcnn/cascade_r50_fpn.py \
 #     --work-dir ../work_dirs/cascade_r50_fpn_batch_8 \
@@ -6,21 +8,50 @@
 #     --wandb_tag cascade_rcnn \
 #     --batch_size 8
 
+# python tools/train.py \
+#     configs/custom/cascade_rcnn/cascade_r50_fpn.py \
+#     --work-dir ../work_dirs/cascade_r50_fpn_batch_16 \
+#     --seed 42 \
+#     --wandb_nm cascade_r50_fpn_batch_16 \
+#     --wandb_tag cascade_rcnn \
+#     --batch_size 16
+
+# python tools/train.py \
+#     configs/custom/cascade_rcnn/cascade_r50_fpn.py \
+#     --work-dir ../work_dirs/cascade_r50_fpn_batch_32 \
+#     --seed 42 \
+#     --wandb_nm cascade_r50_fpn_batch_32 \
+#     --wandb_tag cascade_rcnn \
+#     --batch_size 32
+
+## cascadercnn_r101 experiment
+
 python tools/train.py \
-    configs/custom/cascade_rcnn/cascade_r50_fpn.py \
+    configs/custom/cascade_rcnn/cascade_r101_fpn.py \
+    --work-dir ../work_dirs/cascade_r50_fpn_batch_8 \
+    --seed 42 \
+    --wandb_nm cascade_r50_fpn_batch_8 \
+    --wandb_tag cascade_rcnn \
+    --batch_size 8
+    --epochs 20
+
+python tools/train.py \
+    configs/custom/cascade_rcnn/cascade_r101_fpn.py \
     --work-dir ../work_dirs/cascade_r50_fpn_batch_16 \
     --seed 42 \
     --wandb_nm cascade_r50_fpn_batch_16 \
     --wandb_tag cascade_rcnn \
     --batch_size 16
+    --epochs 40
 
 python tools/train.py \
-    configs/custom/cascade_rcnn/cascade_r50_fpn.py \
+    configs/custom/cascade_rcnn/cascade_r101_fpn.py \
     --work-dir ../work_dirs/cascade_r50_fpn_batch_32 \
     --seed 42 \
     --wandb_nm cascade_r50_fpn_batch_32 \
     --wandb_tag cascade_rcnn \
     --batch_size 32
+    --epochs 80
 
 # python tools/train.py configs/custom/cascade_rcnn/pvt_b1.py --work-dir work_dirs/cascade_rcnn_pvt_b1 --seed 42
 
