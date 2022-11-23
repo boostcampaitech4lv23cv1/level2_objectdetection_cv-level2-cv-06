904db52c2aba65ad64f4beebe4df1560a224e5bc
Ju Heon Oh <ojhgold@gmail.com>
2022-11-23 16:11:21 +0000

    add: resnet50 (detr,deformable_detr)

--- mmdetection/configs/custom/deformable_detr/resnet.py
+++ mmdetection/configs/custom/deformable_detr/resnet.py
@@ -1,59 +1,70 @@
 _base_ = [
     "../_base_/dataset.py",
-    "../_base_/base_scheduler.py",
     "../_base_/runtime.py",
+    "../_base_/base_scheduler.py",
 ]
 
+
 model = dict(
-    type="DETR",
+    type="DeformableDETR",
     backbone=dict(
         type="ResNet",
         depth=50,
         num_stages=4,
-        out_indices=(3,),
+        out_indices=(1, 2, 3),
         frozen_stages=1,
         norm_cfg=dict(type="BN", requires_grad=False),
         norm_eval=True,
         style="pytorch",
         init_cfg=dict(type="Pretrained", checkpoint="torchvision://resnet50"),
     ),
+    neck=dict(
+        type="ChannelMapper",
+        in_channels=[512, 1024, 2048],
+        kernel_size=1,
+        out_channels=256,
+        act_cfg=None,
+        norm_cfg=dict(type="GN", num_groups=32),
+        num_outs=4,
+    ),
     bbox_head=dict(
-        type="DETRHead",
+        type="DeformableDETRHead",
+        num_query=300,
         num_classes=10,
         in_channels=2048,
+        sync_cls_avg_factor=True,
+        as_two_stage=False,
         transformer=dict(
-            type="Transformer",
+            type="DeformableDetrTransformer",
             encoder=dict(
                 type="DetrTransformerEncoder",
                 num_layers=6,
                 transformerlayers=dict(
                     type="BaseTransformerLayer",
+                    attn_cfgs=dict(
+                        type="MultiScaleDeformableAttention", embed_dims=256
+                    ),
+                    feedforward_channels=1024,
+                    ffn_dropout=0.1,
+                    operation_order=("self_attn", "norm", "ffn", "norm"),
+                ),
+            ),
+            decoder=dict(
+                type="DeformableDetrTransformerDecoder",
+                num_layers=6,
+                return_intermediate=True,
+                transformerlayers=dict(
+                    type="DetrTransformerDecoderLayer",
                     attn_cfgs=[
                         dict(
                             type="MultiheadAttention",
                             embed_dims=256,
                             num_heads=8,
                             dropout=0.1,
-                        )
+                        ),
+                        dict(type="MultiScaleDeformableAttention", embed_dims=256),
                     ],
-                    feedforward_channels=2048,
-                    ffn_dropout=0.1,
-                    operation_order=("self_attn", "norm", "ffn", "norm"),
-                ),
-            ),
-            decoder=dict(
-                type="DetrTransformerDecoder",
-                return_intermediate=True,
-                num_layers=6,
-                transformerlayers=dict(
-                    type="DetrTransformerDecoderLayer",
-                    attn_cfgs=dict(
-                        type="MultiheadAttention",
-                        embed_dims=256,
-                        num_heads=8,
-                        dropout=0.1,
-                    ),
-                    feedforward_channels=2048,
+                    feedforward_channels=1024,
                     ffn_dropout=0.1,
                     operation_order=(
                         "self_attn",
@@ -67,14 +78,10 @@ model = dict(
             ),
         ),
         positional_encoding=dict(
-            type="SinePositionalEncoding", num_feats=128, normalize=True
+            type="SinePositionalEncoding", num_feats=128, normalize=True, offset=-0.5
         ),
         loss_cls=dict(
-            type="CrossEntropyLoss",
-            bg_cls_weight=0.1,
-            use_sigmoid=False,
-            loss_weight=1.0,
-            class_weight=1.0,
+            type="FocalLoss", use_sigmoid=True, gamma=2.0, alpha=0.25, loss_weight=2.0
         ),
         loss_bbox=dict(type="L1Loss", loss_weight=5.0),
         loss_iou=dict(type="GIoULoss", loss_weight=2.0),
@@ -83,16 +90,28 @@ model = dict(
     train_cfg=dict(
         assigner=dict(
             type="HungarianAssigner",
-            cls_cost=dict(type="ClassificationCost", weight=1.0),
+            cls_cost=dict(type="FocalLossCost", weight=2.0),
             reg_cost=dict(type="BBoxL1Cost", weight=5.0, box_format="xywh"),
             iou_cost=dict(type="IoUCost", iou_mode="giou", weight=2.0),
         )
     ),
     test_cfg=dict(max_per_img=100),
 )
+data = dict(
+    samples_per_gpu=64,
+    workers_per_gpu=4,
+)
 optimizer = dict(
     type="AdamW",
     lr=1e-3,
+    weight_decay=0.0001,
+    paramwise_cfg=dict(
+        custom_keys={
+            "backbone": dict(lr_mult=0.1),
+            "sampling_offsets": dict(lr_mult=0.1),
+            "reference_points": dict(lr_mult=0.1),
+        }
+    ),
 )
 lr_config = dict(
     _delete_=True,
@@ -105,3 +124,6 @@ lr_config = dict(
     by_epoch=False,
     min_lr=1e-6,
 )
+
+optimizer_config = dict(_delete_=True, grad_clip=dict(max_norm=0.1, norm_type=2))
+runner = dict(type="EpochBasedRunner", max_epochs=150)
