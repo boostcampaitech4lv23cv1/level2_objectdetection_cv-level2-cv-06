0752078cbd6e6ec9216c72079f14b52738567ef9
JuHeon Oh <ojhgold@gmail.com>
2022-11-24 18:28:11 +0900

    refactor: black & fix func args
    apply black
    fix increment_path args(sep)

---
+++ yolov7/run/run.sh
@@ -0,0 +1,7 @@
+# finetune p5 models
+python train.py --workers 8 --device 0 --batch-size 32 --data data/coco.yaml --img 640 640 --cfg cfg/training/yolov7.yaml --weights 'yolov7_training.pt' --name yolov7 --hyp data/hyp.scratch.custom.yaml
+# train p5 models
+python -m torch.distributed.launch --nproc_per_node 4 --master_port 9527 train.py\
+--workers 8 --device 0,1,2,3 --sync-bn --batch-size 128 --data data/coco.yaml --img 640 640\
+--cfg cfg/training/yolov7.yaml --weights 'yolov7_training.pt' --name yolov7 --hyp data/hyp.scratch.p5.yaml
+
