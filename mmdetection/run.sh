d2a8d197eec119c13e760b93281ea035667be556
sshinohs <sshinohs@gmail.com>
2022-11-24 11:38:15 +0000

    test: score threshold

--- mmdetection/run.sh
+++ mmdetection/run.sh
@@ -7,15 +7,6 @@
 # python tools/train.py configs/custom/cascade_rcnn/eff_b0.py
 
 python tools/train.py \
-    configs/custom/cascade_rcnn/cascade_r50_fpn_score_thr_dot01.py \
-    --work-dir ../work_dirs/cascade_r50_fpn_batch_8_score_thr_dot01 \
-    --seed 42 \
-    --wandb_nm cascade_r50_fpn_batch_8_score_thr_dot01 \
-    --wandb_tag cascade_rcnn \
-    --batch_size 8 \
-    --epochs 20
-
-python tools/train.py \
     configs/custom/cascade_rcnn/cascade_r50_fpn_score_thr_dot03.py \
     --work-dir ../work_dirs/cascade_r50_fpn_batch_8_score_thr_dot03 \
     --seed 42 \
@@ -23,3 +14,21 @@ python tools/train.py \
     --wandb_tag cascade_rcnn \
     --batch_size 8 \
     --epochs 20
+
+# python tools/train.py \
+#     configs/custom/cascade_rcnn/cascade_r50_fpn_score_thr_dot00.py \
+#     --work-dir ../work_dirs/cascade_r50_fpn_batch_8_score_thr_dot00 \
+#     --seed 42 \
+#     --wandb_nm cascade_r50_fpn_batch_8_score_thr_dot00 \
+#     --wandb_tag cascade_rcnn \
+#     --batch_size 8 \
+#     --epochs 20
+
+# python tools/train.py \
+#     configs/custom/cascade_rcnn/cascade_r50_fpn_score_thr_dot01.py \
+#     --work-dir ../work_dirs/cascade_r50_fpn_batch_8_score_thr_dot01 \
+#     --seed 42 \
+#     --wandb_nm cascade_r50_fpn_batch_8_score_thr_dot01 \
+#     --wandb_tag cascade_rcnn \
+#     --batch_size 8 \
+#     --epochs 20
