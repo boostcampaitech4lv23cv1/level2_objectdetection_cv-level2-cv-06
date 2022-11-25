150f85ec55456482b618b2805bee9998fee83882
tec10182 <tec10182@naver.com>
2022-11-25 06:31:07 +0000

    feat: best mmdetection model

--- mmdetection/run.sh
+++ mmdetection/run.sh
@@ -1,7 +1,7 @@
 # python tools/train.py configs/custom/cascade_rcnn/pvt_b1.py --work-dir work_dirs/cascade_rcnn_pvt_b1 --seed 42
 
-python tools/train.py configs/custom/atss/resnet50.py \
---wandb_nm atss_res50_batch_64 --wandb_tag atss resnet
+python tools/train.py configs/custom/cascade_rcnn/swinv2_large.py \
+--wandb_nm cascade_swinv2_large_augmentation --wandb_tag cascade augmentation swinv2_large
 
 
-python tools/train.py configs/custom/cascade_rcnn/eff_b0.py
+
