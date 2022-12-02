8cc9b1c86518a06d6bd5a09d1b853e819755ea2d
Juheon Oh <61729168+OZOOOOOH@users.noreply.github.com>
2022-12-02 16:53:57 +0900

    Delete yolov5 directory

--- yolov5/convert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-for ((i=0; i<=4; i++))
-do
-    python runs/convert2csv.py --file yolov5x_kfold${i}_TTA
-    python runs/convert2csv.py --file yolov5x_kfold${i}
-done
--- yolov5/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-for ((i=0; i<=4; i++))
-do
-    python train.py --batch 64 --data trash${i}.yaml --weights yolov5x.pt --device 0 --name yolov5x_kfold${i} --img 640 \
-    --epochs 300 --multi-scale --seed 42 --sync-bn --cache --patience 10
-done
